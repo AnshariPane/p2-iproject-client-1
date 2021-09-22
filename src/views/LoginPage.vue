6457a4261b6ec397a2127261b135eaa0f85bc19f
AnshariPane <legion.voidgenome@gmail.com>
2021-09-22 20:20:13 +0700

    Navbar

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -51,7 +51,7 @@ export default {
         async loginButton() {
             const payload = this.credentials;
             await this.loginHandler(payload);
-            //    this.$router.push("/")
+            this.$router.push("/");
         },
     },
 };
